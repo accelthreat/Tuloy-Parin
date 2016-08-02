025ae90e660964d64ef33e128ce4d0664a11dafb
accelthreat <matthewlemuelr@gmail.com>
2016-08-02 18:14:49 +0800

    Updated

--- tuloyparin5/tuloyparin5.ino
+++ tuloyparin5/tuloyparin5.ino
@@ -14,22 +14,15 @@ const int S = Q / 4;          // 1/16 Note
 
 Piezzo piezzo (PiezzoPin); // Create a Piezzo object then assign to pin 8
 //Format:  int MusicPiece [] = {NOTE,BEAT,NOTE,BEAT,NOTE,BEAT,............};
-int Stanza [] = {F4, E, F4, E, C5, E, D5, Q , F4, E, F4, E + TE , F4, E + TE, F4, E  , DS5, E, D5, E, D5, Q, D5, E, G5, E, F5, E, F5, E, F5, E, D5, E, C5, Q + E, C5, Q + E, R, H};
-int Stanza2 [] = {AS4, TE + E, AS4, TE + E, AS4, TE + E, R , Q, AS4, E, D5, E, D5, E, D5, E, D5, E, C5, E, R, Q, F4, Q, F5, Q, D5, Q, C5, Q, AS4, E, G4, E, R, H};
-int Stanza3 [] = {F4, TE + S, AS4, TE + S, AS4, TE + S, AS4, TE + S, R, E, F4, E, D5, E, DS5, E, D5, E, C5, E, R, Q, F4, E, G4, E, R, E, D5, E, R, E, D5, E, D5, E, C5, E, C5, Q + E, R, H + E};
-int Chorus [] = {F4, E, G5, E, F5, E, F5, E, D5, E, G5, E, F5, E, F5, E + TE + S, F5, E + TE + S, F5, E + TE + S, R, Q};
-int Chorus2 [] = {F4, E, G5, E, F5, E, F5, E, D5, E, DS5, E, D5, E, D5, E, C5, Q, D5,  Q, C5, E, AS4, E, R, Q};
-int Chorus3 [] = {D5, E, D5, E, D5, E, D5, E, DS5, E, D5, Q, C5, Q, AS4, E + S, R, Q};  
+double Stanza [] = {F4, E, F4, E, C5, E, D5, Q , F4, E, F4, E + TE , F4, E + TE, F4, E  , DS5, E, D5, E, D5, Q, D5, E, G5, E, F5, E, F5, E, F5, E, D5, E, C5, Q + E, C5, Q + E, R, H, END};
+double Stanza2 [] = {AS4, TE + E, AS4, TE + E, AS4, TE + E, R , Q, AS4, E, D5, E, D5, E, D5, E, D5, E, C5, E, R, Q, F4, Q, F5, Q, D5, Q, C5, Q, AS4, E, G4, E, R, H, END};
+double Stanza3 [] = {F4, TE + S, AS4, TE + S, AS4, TE + S, AS4, TE + S, R, E, F4, E, D5, E, DS5, E, D5, E, C5, E, R, Q, F4, E, G4, E, R, E, D5, E, R, E, D5, E, D5, E, C5, E, C5, Q + E, R, H + E, END};
+double Chorus [] = {F4, E, G5, E, F5, E, F5, E, D5, E, G5, E, F5, E, F5, E + TE + S, F5, E + TE + S, F5, E + TE + S, R, Q, END};
+double Chorus2 [] = {F4, E, G5, E, F5, E, F5, E, D5, E, DS5, E, D5, E, D5, E, C5, Q, D5,  Q, C5, E, AS4, E, R, Q , END};
+double Chorus3 [] = {D5, E, D5, E, D5, E, D5, E, DS5, E, D5, Q, C5, Q, AS4, E + S, R, Q , END};
 
-//Get the arrays' lengths
-int Chorusl = sizeof(Chorus) / sizeof(int);  
-int Chorus2l = sizeof(Chorus2) / sizeof(int);
-int Chorus3l = sizeof(Chorus3) / sizeof(int);
-int Stanzal = sizeof(Stanza) / sizeof(int);
-int Stanza2l = sizeof(Stanza2) / sizeof(int);
-int Stanza3l = sizeof(Stanza3) / sizeof(int);
 
-int debug = 1; //Debug Mode 
+int debug = 1; //Debug Mode
 char a;
 
 void setup() {
@@ -43,13 +36,13 @@ void loop() {
   {
     a = Serial.read();
     if (a  == 'p') {
-      piezzo.play(Stanza, Stanzal);
-      piezzo.play(Stanza2, Stanza2l);
-      piezzo.play(Stanza3, Stanza3l);
-      piezzo.play(Chorus, Chorusl);
-      piezzo.play(Chorus2, Chorus2l);
-      piezzo.play(Chorus, Chorusl);
-      piezzo.play(Chorus3, Chorus3l);
+      piezzo.play(Stanza);
+      piezzo.play(Stanza2);
+      piezzo.play(Stanza3);
+      piezzo.play(Chorus);
+      piezzo.play(Chorus2);
+      piezzo.play(Chorus);
+      piezzo.play(Chorus3);
     }
   }
 
